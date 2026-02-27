471292490530f571793523b488aa15d0caf2282f
Mahmood Ramzani <mahmoodramzani@gmail.com>
2026-02-28 00:36:18 +0330

    Add react-native.config.js for vector icons configuration

---
+++ mobile/react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        android: null, // autolinking handles the native module
+      },
+    },
+  },
+  assets: ['./node_modules/react-native-vector-icons/Fonts'],
+};
